a129b097688183e0472c8623dd3436cee29bf550
LakeFox <mason@lakefox.net>
2023-10-13 18:54:40 -0500

    canvas

--- relay/client/Term.js
+++ relay/client/Term.js
@@ -28,8 +28,7 @@ function Term(socket) {
         let fitAddon = new FitAddon.FitAddon();
         term.loadAddon(fitAddon);
         term.open(el);
-
-        // fitAddon.fit();
+        term.loadAddon(new CanvasAddon.CanvasAddon());
 
         // Initialize the ResizeObserver
         const resizeObserver = new ResizeObserver((entries) => {
