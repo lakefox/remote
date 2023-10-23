cf12b2b6b4155a739db7ae9d5b49daf623259301
LakeFox <mason@lakefox.net>
2023-10-23 18:17:21 -0500

    d

--- remote/main.js
+++ remote/main.js
@@ -7,7 +7,7 @@ let io = new Auth(ws);
 console.log("Socket is up and running...");
 
 io.on("open", (socket) => {
-    console.log("Connected");
+    console.log("Connected", socket.id);
     // socket.emit("upgrade", { value: true });
 
     socket.on("channel", (channel) => {
